1ad8543a494661dc08d2d7a76ddbddd410bb41ec
chhaknak <chanchhaknak@gmail.com>
2020-01-31 22:45:13 +0700

    kshrd-final-project-2020

--- Amazing Khmer Project - Final/js/scrollToTop.js
+++ Amazing Khmer Project - Final/js/scrollToTop.js
@@ -1,23 +1,15 @@
-window.onscroll = function () {
-    scrollTopChecking();
-}
-
-let _btn = document.getElementById('btn');
-
-function scrollTopChecking() {
-    //console.log(document.documentElement.scrollTop);
-    if (document.documentElement.scrollTop >= 450 || document.body.scrollTop >= 450) {
-        _btn.style.display = 'block';
-    } else {
-        _btn.style.display = 'none';
-    }
-}
-
-btn.onclick = goToTop;
-
-function goToTop() {
-    // chrome
-    document.documentElement.scrollTop = 0;
-    // safari
-    document.body.scrollTop = 0;
-}
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#btn').fadeIn();
+        } else {
+            $('#btn').fadeOut();
+        }
+    });
+    $('#btn').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+});
